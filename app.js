afd0e3e7799f026604de7b9d015275331904db75
Paola Garcia Munoz <pgarciamunoz@twilio.com>
2022-11-24 12:54:09 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Imports
+const express = require('express');
+const app = express();
+
+
+
+app.use(express.static('public'));
+
+// Set View's
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/about', (req, res) => {
+       res.render('about');
+    })
+
+
+
+// Listen on Port 5000
+const port = 5000;
+app.listen(port, () => console.info(`App listening on port ${port}`))
